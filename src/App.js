88ca745c498f9b483689380e30485ab573204063
Arshed Ahmed <arshedahmedmail@gmail.com>
2022-02-16 23:10:48 +0530

    add refs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, { useRef } from "react";
+
 function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const submitRef = useRef(null);
   return (
     <div className="App">
-      <input type="text" ref={fostNamdRef} placeholder="Enter First Name" />
-      <input type="text" ref={lastNamdRef} placeholder="Enter First Name" />
+      <input type="text" ref={firstNameRef} placeholder="Enter First Name" />
+      <input type="text" ref={lastNameRef} placeholder="Enter First Name" />
       <input type="text" placeholder="Enter Last Name" />
       <button ref={submitRef}>submit</button>
     </div>
